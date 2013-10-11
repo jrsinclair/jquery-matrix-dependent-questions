cfd15ec997523c71d736ef4a0bcbda8060227c46
James Sinclair <jsinclair@squiz.com.au>
2013-10-11 15:02:50 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# Dependent Questions jQuery Plugin
+
+**Make a form question's visibility dependent on some other question simply by adding `data-depends-on="inputName=value"` to your wrapper HTML element. No other code required.**
+
+This plugin allows you to specify that a particular form question should only be
+made visible if the answer to a previous question is a specific value. It does this
+by making use of HTML5 data attributes. For example:
+
+`````html
+<form action="?" id="myform">
+  <fieldset class="form-question">
+    <legend>Would you like to see another question?</legend>
+    <span>
+      <input type="radio" name="more" value="no" id="more-no"/>
+      <label for="more-no">No</label>
+    </span>
+    <span>
+      <input type="radio" name="more" value="yes" id="more-yes"/>
+      <label for="more-yes">Yes</label>
+    </span>
+  </fieldset>
+  <fieldset data-depends-on="more=yes">
+    <div class="form-question">
+      <label for="extra">What else would you like to add?</label>
+      <input type="text" name="extra" id="extra"/>
+    </div>
+  </fieldset>
+</form>
+`````
+
+In the above example, the question "What else would you like to add?" would only be
+displayed when the first question is answered as "yes".
+
+Note that the `data-depends-on` attribute refers to the form input **name**, not an ID.
+
+To use plugin, add something like the following to your web page:
+
+`````html
+<script src="http://code.jquery.com/jquery.min.js"></script>
+<script src="jquery.dependent-questions.js"></script>
+<script>
+  $('#myform').dependentQuestions();
+</script>
+`````
+## Options
+
+You can specify how the dependent questions are revealed by passing options to the plugin constructor. For example, if you would like the questions to fade in, rather than slide down, you could specify something like the following:
+
+`````javascript
+$("myform").dependentQuestions({
+    effect: "fade",    // This can be 'fade' or 'slide'
+    duration: "slow"   // This can be 'slow', 'fast', or a number of milliseconds
+});
+`````
+
+## Thanks
+
+Development of this plugin was supported by [Squiz](http://squiz.com.au).
---
+++ jquery.matrix-dependent-questions.js
@@ -0,0 +1,201 @@
+/*!
+ * Dependent questions jQuery plugin.
+ *
+ * Author: jsinclair@squiz.com.au
+ *
+ * Written using boilerplate code from
+ * http://coding.smashingmagazine.com/2011/10/11/essential-jquery-plugin-patterns/
+ *
+ * © 2012 James Sinclair
+ * MIT License. 
+ */
+
+/*jslint nomen:true, browser:true*/
+/*globals jQuery*/
+;
+(function ($, window, document, undefined) {
+    "use strict";
+    // See http://bit.ly/rc0Nzl for reasoning on why window, document and undefined
+    // are passed in here.
+
+
+    // The some() method is not implemented in some browsers.
+    // See https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Array/some
+    // for the origin of this polyfill.
+    if (!Array.prototype.some) {
+        Array.prototype.some = function (fun, thisp) {
+            var t, len, i;
+            if (this === null) { throw new TypeError(); }
+
+            t   = Object(this);
+            len = t.length >>> 0;
+            if (typeof fun !== "function") { throw new TypeError(); }
+
+            for (i = 0; i < len; i += 1) {
+                if (i in t && fun.call(thisp, t[i], i, t)) { return true; }
+            }
+            return false;
+        };
+    }
+
+    // Return true if haystack contains needle 
+    function contains(haystack, needle) {
+        function equals(haystrand) {
+            return haystrand === needle;
+        }
+        return haystack.some(equals);
+    }
+
+
+    // Configuration and defaults.
+    var pluginName = 'dependentQuestions',
+        defaults = {
+            "effect":   "slide", // Must be one of 'slide' or 'fade'
+            "duration": "fast"
+        },
+        effectsMap = {
+            "slide": {
+                "show": "slideDown",
+                "hide": "slideUp"
+            },
+            "fade": {
+                "show": "fadeIn",
+                "hide": "fadeOut"
+            }
+        };
+
+    // The actual plugin constructor
+    function QuestionToggler(element, options) {
+        this.element = element;
+
+        // Handle options.
+        this.options   = $.extend({}, defaults, options);
+        this._defaults = defaults;
+        this._name     = pluginName;
+
+        this.init();
+    }
+
+    // Initialisation. Parses all the 'data-depends-on' attributes and sets event
+    // listeners.
+    QuestionToggler.prototype.init = function () {
+        // Find all elements that have a 'data-depends-on' attribute.
+        var questions, dataRe, dependentsMap, effect, duration;
+
+        effect        = this.options.effect;
+        duration      = this.options.duration;
+        dependentsMap = {};
+
+        // Regular expression for parsing data attribute.
+        dataRe = new RegExp('([^=]*)=(.*)');
+
+        // Fetch all the elements with 'data-depends-on' attributes.
+        if (!this.element.find) { this.element = $(this.element); }
+        questions = this.element.find('[data-depends-on]').parents('.sq-form-question');
+        questions.css('background', 'pink');
+
+        // Get the value of a form element by name, even if it is a radio button
+        // or checkbox
+        function getValueByName(inputName) {
+            var multiInputs, el, elType, multi, modifier, checkVals;
+            multiInputs = ['radio', 'checkbox'];
+            el = $('[name="' + inputName + '"]');
+            if (el.length === 0) { return undefined; }
+
+            elType = el.attr('type');
+            multi  = contains(multiInputs, elType);
+            if (!multi) { return el.val(); }
+
+            if (elType === 'radio') { return el.filter(':checked').val(); }
+
+            if (elType === 'checkbox') {
+                checkVals = $.map(el.filter(':checked'), function (checkbox) {
+                    return $(checkbox).val();
+                });
+                return checkVals;
+            }
+        }
+
+        // Event handler for when a toggling element changes value.
+        function onTogglerChange(evt) {
+            var toggler, dependents, showVal, val, show, effectFunc, action;
+            toggler    = $(evt.target);
+            dependents = toggler.data('dependents');
+            showVal    = toggler.data('showVal');
+            val        = getValueByName(toggler.attr('name'));
+            show       = (typeof val === 'object')
+                       ? contains(val, showVal)
+                       : (val === showVal);
+            action     = (show) ? 'show' : 'hide';
+            effectFunc = effectsMap[effect][action];
+            dependents[effectFunc](duration);
+        }
+
+        // Create a map of things that need to be toggled.
+        function addMapEntry(i, element) {
+            var data, el;
+            el   = $(element);
+            data = el.find('[data-depends-on]').data('depends-on');
+            if (!dependentsMap[data]) {
+                dependentsMap[data] = el;
+            } else {
+                dependentsMap[data] = el.add(dependentsMap[data]);
+            }
+        }
+        questions.each(addMapEntry);
+
+        // Create event listeners
+        function setListener(key, dependents) {
+            var matches, name, showVal, toggler;
+            // Parse the key value
+            matches = dataRe.exec(key);
+            if (!matches) { return; }
+            name    = matches[1];
+            showVal = matches[2];
+
+            // Grab the relevant elements
+            toggler = $('[name="' + name + '"]');
+            toggler.data('dependents', dependents);
+            toggler.data('showVal', showVal);
+            toggler.change(onTogglerChange);
+        }
+        $.each(dependentsMap, setListener);
+
+        // Set the initial state of each dependent question to match form state.
+        function showHideByValue(key, dependents) {
+            var matches, name, showVal, toggler, val, func, show;
+            // Parse the key value
+            matches = dataRe.exec(key);
+            if (!matches) { return; }
+            name    = matches[1];
+            showVal = matches[2];
+
+            // Work out whether or not to show the value
+            toggler = $('[name="' + name + '"]');
+            val     = getValueByName(name);
+            if (typeof val === 'object') {
+                show = contains(val, showVal);
+            } else {
+                show = (val === showVal);
+            }
+            func = (show) ? 'show' : 'hide';
+            dependents[func]();
+        }
+        $.each(dependentsMap, showHideByValue);
+    };
+
+    // A really lightweight plugin wrapper around the constructor, 
+    // preventing against multiple instantiations
+    $.fn[pluginName] = function (options) {
+        return this.each(function () {
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(
+                    this,
+                    'plugin_' + pluginName,
+                    new QuestionToggler(this, options)
+                );
+            }
+        });
+    };
+
+}(jQuery, window, document));
